0e68e6eefb264d074553ab3c201e00d31a8658d7
Calvin Neo <calvinneo1995@gmail.com>
2025-06-26 22:46:55 +0800

    Use dot instead of gv
    Signed-off-by: Calvin Neo <calvinneo1995@gmail.com>

--- gen.py
+++ gen.py
@@ -3,7 +3,23 @@ import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.text import Text
 import matplotlib.patches as patches
+import textwrap
 import re
+
+def pretty_size(size, units=('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')):
+    if size == 0:
+        return "0B"
+    
+    unit_index = 0
+    while size >= 1024 and unit_index < len(units) - 1:
+        size /= 1024.0
+        unit_index += 1
+    
+    return f"{size:.2f}{units[unit_index]}"
+
+# 示例
+size_in_bytes = 1234567890
+print(pretty_size(size_in_bytes))  # 输出：1.15GB
 
 def gen_image1(chains_with_weights):
     # # 输入的链表和权值
@@ -28,17 +44,19 @@ def gen_image1(chains_with_weights):
 
     # G = nx.petersen_graph()
     # 使用 spring_layout 作为初始布局
-    pos = nx.nx_agraph.graphviz_layout(G, args="-Goverlap=compress -Nshape=box -LU0.05")
+    # pos = nx.nx_agraph.graphviz_layout(G, args="-Goverlap=compress -Nshape=box")
+
+    H = nx.convert_node_labels_to_integers(G, label_attribute="node_label")
+    H_layout = nx.nx_pydot.pydot_layout(H, prog="dot")
+    pos = {H.nodes[n]["node_label"]: p for n, p in H_layout.items()}
 
     # pos = {k: (v[0]/10, v[1]/10) for (k, v) in pos.items()}
-
 
     x_values = [pos[node][0] for node in G.nodes()]
     y_values = [pos[node][1] for node in G.nodes()]
     x_range = max(x_values) - min(x_values)
     y_range = max(y_values) - min(y_values)
     print("[{},{}] - [{},{}]".format(min(x_values), max(x_values), min(y_values), max(y_values)))
-
 
 
     # 计算图形的边界范围
@@ -48,13 +66,16 @@ def gen_image1(chains_with_weights):
     fig, ax = plt.subplots(figsize=(x_range * 0.2, y_range * 0.2))
     # 绘制有向图
     # nx.draw(G, pos, node_shape = "s", node_color='lightblue', alpha = 0.5, edge_color='gray', node_size=10000, arrows=True, arrowsize=16)
-    nx.draw(G, pos, alpha = 1, edge_color='black', arrows=True, arrowsize=24)
+    nx.draw(G, pos, alpha = 1, edge_color='black', arrows=True, arrowsize=36)
 
-    labels = {node: f"{node[:50]} ({node_weights[node]})" for node in G.nodes()}
-    nx.draw_networkx_labels(G, pos, labels=labels, font_size=24)  # 调整字体大小以避免重叠
+    def nice(x):
+        return textwrap.fill(x[1:-1], width=30)
+    labels = {node: f"{nice(node)}\n({pretty_size(node_weights[node])})" for node in G.nodes()}
+
+    nx.draw_networkx_labels(G, pos, labels=labels, font_size=36)  # 调整字体大小以避免重叠
 
     # 调整布局以避免箭头重叠
-    # plt.tight_layout()
+    plt.tight_layout()
 
     # 保存为 SVG 文件
     plt.savefig("directed_graph_with_weights.svg", format="svg")
@@ -73,7 +94,7 @@ def parse_log(file_path):
         log_content = file.read()
 
     def handle(x):
-        return x.strip('\t').split("+")[0]
+        return "\"" + x.strip('\t').split("+")[0] + "\""
 
     # 使用正则表达式匹配日志内容
     for match in pattern.finditer(log_content):
